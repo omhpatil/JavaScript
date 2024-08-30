69ddabe9a0d9df88b76913381d1b893b2f320d8c
Om Patil <97589129+omhpatil@users.noreply.github.com>
2024-08-30 10:33:41 +0530

    Add files via upload

---
+++ 021. ForOf.js
@@ -0,0 +1,15 @@
+// For Of Keyword
+
+// const arr = [1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+const name = "Om Patil"
+
+for (const char of name) {
+    if(char == " ")
+        continue
+    console.log(char);
+}
